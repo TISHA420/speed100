91cbb7ee0ab889a3c50a379f4aa661f03151c54a
TISHA420 <89423319+TISHA420@users.noreply.github.com>
2021-10-14 13:20:53 +0600

    Add files via upload

---
+++ logo.sh
@@ -0,0 +1,17 @@
+#bin/bash
+cd speed
+chmod 777 logo.sh
+pkg install php |lolcat
+pkg install weget |lolcat
+pkg install curl |lolcat
+pkg install python |lolcat
+pkg install python2 |lolcat
+pkg install git |lolcat
+pkg install figlet |lolcat
+pkg install ruby |lolcat
+gem install lolcat |lolcat
+pkg install mpv |lolcat
+pip install --upgrade pip |lolcat
+figlet Black |lolcat
+echo $clear
+
